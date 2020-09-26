0df3145f058af1e157acf10804724008df21f885
NitinLad <71937061+NitinLad@users.noreply.github.com>
2020-09-26 22:38:46 +0530

    Add files via upload

---
+++ EditTitle.ps1
@@ -0,0 +1,5 @@
+﻿Set-ExecutionPolicy RemoteSigned 
+$LiveCred = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection 
+Import-PSSession $Session 
+import-csv C:\Users\nlad\Desktop\PANACEA\Title.csv | foreach {Set-User –identity $_."identity" -Title $_."title"}
